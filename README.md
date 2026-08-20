ea47fc849726b1ae0193fec01f00c318b7c713d5
Seungki Kim <tttuuu888@gmail.com>
2026-08-20 23:55:50 +0900

    Rewrite janet-completion
    - Remove janet-complete module dependency.
    - Add the globalized minor mode 'janet-completion-mode'.

--- README.md
+++ README.md
@@ -1,38 +1,30 @@
 # janet-completion
 
-`janet-completion.el` provides completion support for Janet by communicating with the `janet-complete` module in a running `inf-janet` REPL.
+`janet-completion.el` provides completion support for Janet by communicating
+with the running `inf-janet` REPL.
 
-Completion candidates are retrieved from the REPL and can be used in both the REPL buffer and in Janet source files connected to the REPL.
+Candidates are generated using Janet's built-in `all-bindings` function.
+
+Completion works in the REPL buffer and in Janet source files.
 
 ## Requirements
 
-1. Install the `janet-complete` module for Janet:
-
-    ```sh
-    [sudo] jpm install https://github.com/tttuuu888/janet-complete.git
-    ```
-
-2. Ensure the REPL has loaded the module by evaluating `(use complete)`.
-
-    You can automate this by setting the Janet profile:
-    
-    ```sh
-    export JANET_PROFILE=$HOME/.config/janet/init.janet
-    ```
-    
-    And adding the following to your init.janet:
-    
-    ```
-    (use complete)
-    ```
+- `inf-janet`
+- A Janet version that has the `all-bindings` function
 
 ## Installation
 
-Install `janet-completion.el` manually or via vc-use-package (Emacs 30+):
+Install `janet-completion.el` manually, or via `vc-use-package` (Emacs 30+).
+Then enable `janet-completion-mode`:
 
 ```emacs-lisp
 (use-package janet-completion
   :vc
-  (:url "https://github.com/tttuuu888/janet-completion" :branch "main" :rev :newest))
+  (:url "https://github.com/tttuuu888/janet-completion" :branch "main" :rev :newest)
+  :config
+  (janet-completion-mode 1))
 
 ```
+
+`janet-completion-mode` is a global minor mode that enables completion in
+`janet-mode` and `inf-janet` REPL buffers.
--- janet-completion.el
+++ janet-completion.el
@@ -1,10 +1,10 @@
-;;; janet-completion.el --- Provide Janet symbol completion via janet-complete -*- lexical-binding: t -*-
+;;; janet-completion.el --- Completion support for Janet -*- lexical-binding: t -*-
 
-;; Copyright (C) 2025 Seungki Kim
+;; Copyright (C) 2025-2026 Seungki Kim
 
 ;; Author: Seungki Kim <tttuuu888@gmail.com>
 ;; URL: https://github.com/tttuuu888/janet-completion
-;; Version: 0.3.0
+;; Version: 0.4.0
 ;; Package-Requires: ((emacs "28.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -22,84 +22,110 @@
 
 ;;; Commentary:
 
-;; This package provides completion support by communicating with the
-;; `janet-complete` module (a Janet module) running in an `inf-janet` REPL.
+;; This package provides completion support by querying a running
+;; `inf-janet' REPL.
 
-;; Completion candidates are retrieved from the REPL and can be used in both
-;; the REPL buffer and in Janet source files connected to the REPL.
+;; Completion works in both the REPL buffer and Janet source files.
+;;
+;; Enable it globally with `janet-completion-mode'.
 
 ;; Requirements:
 
-;; - `janet-complete` (Janet module)
-;; - `inf-janet`
+;; - `inf-janet'
 
 ;;; Code:
 (require 'inf-janet)
 
-(defvar janet-completion-completion-cache nil
-  "Cache for Janet completion results.")
+(defgroup janet-completion nil
+  "Symbol completion for Janet."
+  :group 'janet
+  :prefix "janet-completion-")
+
+(defcustom janet-completion-timeout 0.5
+  "Seconds to wait for the Janet REPL to answer a completion query."
+  :type 'number)
+
+(defcustom janet-completion-minimum-prefix-length 2
+  "Minimum prefix length required to trigger Janet completion."
+  :type 'integer)
+
+(defconst janet-completion--query
+  (concat "(print \"<janet-completion>\""
+          " (string/join (filter |(string/has-prefix? %S $)"
+          " (map string (all-bindings))) \" \")"
+          " \"</janet-completion>\")\n")
+  "Format string to query matching bindings from the Janet REPL.")
+
+(defconst janet-completion--response-regexp
+  "<janet-completion>\\([^\0]*?\\)</janet-completion>"
+  "Regexp for the tagged answer of `janet-completion--query'.")
 
 (defun janet-completion--remove-ansi-escape-sequences (str)
   "Remove ANSI escape sequences from STR."
-  (replace-regexp-in-string
-   "\x1b\\[[0-9;]*m" "" str))
+  (replace-regexp-in-string "\x1b\\[[0-9;]*[a-zA-Z]" "" str))
 
-(defun janet-completion--completion-fetch (prefix)
-  "Send (complete \"PREFIX\") to Janet REPL and return candidate list."
+(defun janet-completion--fetch (prefix)
+  "Fetch bindings starting with PREFIX from the Janet REPL.
+Return a list of completion strings, or nil on timeout."
   (let* ((proc (inf-janet-proc))
-         (output-buffer (get-buffer-create "*janet-completion-output*"))
-         (cmd (format "(complete %S)\n" prefix))
-         (output ""))
-    (with-current-buffer output-buffer
-      (erase-buffer))
-    (let ((original-filter (process-filter proc)))
-      (unwind-protect
-          (progn
-            (set-process-filter
-             proc
-             (lambda (_proc string)
-               (with-current-buffer output-buffer
-                 (insert string))))
-            (process-send-string proc cmd)
-            (accept-process-output proc 0.2)
-            (setq output
-                  (janet-completion--remove-ansi-escape-sequences
-                   (with-current-buffer output-buffer
-                     (buffer-string)))))
-
-        (set-process-filter proc original-filter)))
-    (when (string-match "\\[\\(.*?\\)\\]" output)
-      (let* ((raw (match-string 1 output))
-             (cands (split-string raw "\"" t "\" *, *")))
-        (cl-remove-if (lambda (s) (string-match-p "\\`[ \t\r\n]*\\'" s))
-                      cands)))))
-
-(defun janet-completion-completion-table ()
-  "Return a completion table that fetches completions from Janet."
-  (completion-table-dynamic
-   (lambda (prefix)
-     (or (cdr (assoc prefix janet-completion-completion-cache))
-         (let ((comps (janet-completion--completion-fetch prefix)))
-           (push (cons prefix comps) janet-completion-completion-cache)
-           comps)))))
+         (original-filter (process-filter proc))
+         (deadline (+ (float-time) janet-completion-timeout))
+         (output "")
+         (answer nil))
+    (unwind-protect
+        (progn
+          (set-process-filter
+           proc
+           (lambda (_process string)
+             (setq output (concat output string))))
+          (process-send-string proc (format janet-completion--query prefix))
+          (while (and (not answer) (< (float-time) deadline))
+            (accept-process-output proc 0.05)
+            (when (string-match janet-completion--response-regexp output)
+              (setq answer (match-string 1 output)))))
+      (set-process-filter proc original-filter))
+    (when answer
+      (split-string (janet-completion--remove-ansi-escape-sequences answer)
+                    "[ \t\r\n]+" t))))
 
 (defun janet-completion-completion-at-point ()
-  "Determine the region at point to complete, and return completion data."
-  (let* ((beg (save-excursion (skip-syntax-backward "w_") (point)))
-         (end (save-excursion (skip-syntax-forward "w_") (point))))
-    (when (and (inf-janet-connected-p)
-               (> end beg))
-      (list beg end (janet-completion-completion-table)))))
+  "Return completion data for the symbol at point.
+Return nil if no REPL is connected or if the symbol length is shorter than
+`janet-completion-minimum-prefix-length'."
+  (when-let* (((inf-janet-connected-p))
+              (bounds (bounds-of-thing-at-point 'symbol))
+              (prefix (buffer-substring-no-properties (car bounds) (cdr bounds)))
+              ((>= (length prefix) janet-completion-minimum-prefix-length)))
+    (let ((candidates 'unset))
+      (list (car bounds) (cdr bounds)
+            (completion-table-dynamic
+             (lambda (_string)
+               (when (eq candidates 'unset)
+                 (setq candidates (janet-completion--fetch prefix)))
+               candidates))
+            :exclusive 'no))))
 
-(add-hook 'inf-janet-mode-hook
-          (lambda ()
-            (add-hook 'completion-at-point-functions
-                      #'janet-completion-completion-at-point nil t)))
+(define-minor-mode janet-completion-local-mode
+  "Provide Janet symbol completion in the current buffer.
 
-(add-hook 'janet-mode-hook
-          (lambda ()
-            (add-hook 'completion-at-point-functions
-                      #'janet-completion-completion-at-point nil t)))
+Completion requires a connected `inf-janet' REPL.  Use the global mode
+`janet-completion-mode' to enable this mode in all Janet buffers."
+  :lighter ""
+  (if janet-completion-local-mode
+      (add-hook 'completion-at-point-functions
+                #'janet-completion-completion-at-point nil t)
+    (remove-hook 'completion-at-point-functions
+                 #'janet-completion-completion-at-point t)))
+
+(defun janet-completion--turn-on ()
+  "Turn on `janet-completion-local-mode' in the current buffer."
+  (janet-completion-local-mode 1))
+
+;;;###autoload
+(define-globalized-minor-mode janet-completion-mode
+  janet-completion-local-mode
+  janet-completion--turn-on
+  :predicate '(janet-mode inf-janet-mode))
 
 (provide 'janet-completion)
 ;;; janet-completion.el ends here
